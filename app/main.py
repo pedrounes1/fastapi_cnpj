06509a1dc8e3714cbce72271d5ebbb29d0dd2441
Pedro Nunes <pedrounes@gmail.com>
2021-07-11 13:42:31 -0300

    Criação das rotas de inicialização das tabelas e exlusão das tabelas

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from models.models import inicia, deleta
 
 app = FastAPI()
 
@@ -6,3 +7,21 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return({"msg": "Olá Mundo!"})
+
+
+@app.post('/init_tables/{password}')
+def criaTabelas(password: str):
+    if password != "Arronbydo!":
+        return {"erro": "Senha incorreta cuza1", "senha": password}
+    else:
+        inicia()
+        return {'msg': 'Tabelas criadas com sucesso!'}
+
+
+@app.post('/drop_tables/{password}')
+def dropaTabelas(password: str):
+    if password != "Arronbydo!":
+        return {"erro": "Senha incorreta cuza1", "senha": password}
+    else:
+        deleta()
+        return {'msg': 'Foi pro vinagre!'}
