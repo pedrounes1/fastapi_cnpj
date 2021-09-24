a73c5606fda0d7456627a7498e737b8bd278a811
Pedro Nunes <>
2021-09-24 12:13:40 -0300

    Ajuste do app/seeder.py para: * Trabalhar com chunks de 150 mil dados; * Inserir os dados de todas as UFs, exceto "EX"; * Ampliação da data de fechamento de 2016 para 2013; * Criação da função testaCnaes para validar se todos os cnaes, municipios e situações cadastrais estão cobertos; * Substituição das cnaes canceladas;

--- app/seeder.py
+++ app/seeder.py
@@ -2,11 +2,12 @@ import os
 import logging
 
 import pandas as pd
+from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.session import Session
 
 from models.models import Cidade, Estado, Mesorregiao, Microrregiao, CnaeSecao, CnaeDivisao, CnaeGrupo, CnaeClasse, Cnae, MotivoSituacaoCadastral, NaturezaJuridica, SituacaoCadastral, Porte, Cnpj, deleta, inicia
-from utils.funcoes import criaEngine, seeder
+from utils.funcoes import consultaDados, criaEngine, seeder
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -88,7 +89,7 @@ def postGeral():
                (urls['microrregioes'], Microrregiao), (urls['cidades'], Cidade)]
     cnaes = [(urls['secoes'], CnaeSecao), (urls['divisoes'], CnaeDivisao),
              (urls['grupos'], CnaeGrupo), (urls['classes'], CnaeClasse), (urls['subclasses'], Cnae)]
-    motivo_natureza = [(DIR +'motivos.csv', MotivoSituacaoCadastral),
+    motivo_natureza = [(DIR + 'motivos.csv', MotivoSituacaoCadastral),
                        (DIR + 'naturezas.csv', NaturezaJuridica)]
 
     # Cria as cidades:
@@ -134,34 +135,74 @@ def postCnpjs():
         'codigo_municipio': 'siafi_id',
         'porte_empresa': 'porte_id'}
 
-    with pd.read_csv(arquivo, encoding='utf-8', sep='#', chunksize=50_000, usecols=cols, low_memory=False) as reader:
+    with pd.read_csv(arquivo, encoding='utf-8', sep='#', chunksize=150_000, usecols=cols, low_memory=False) as reader:
         for cont, chunk in enumerate(reader):
             logging.info(f'Inserção CNPJs: Processando o chunk {cont}...')
-            chunk["data_situacao_cadastral"] = pd.to_datetime(
-                chunk["data_situacao_cadastral"], errors='coerce')
-            chunk = chunk.loc[chunk['uf'].str.upper() ==
-                              'PR'].reset_index(drop=True)
-            chunk = chunk.loc[(chunk['situacao_cadastral'] == 2) | (
-                chunk['data_situacao_cadastral'] >= pd.to_datetime('2016-01-01'))].reset_index(drop=True)
+            chunk = chunk.loc[(chunk['uf'].str.upper() != 'EX') & (~chunk['codigo_municipio'].isna())].reset_index(drop=True)
+            chunk["data_situacao_cadastral"] = pd.to_datetime(chunk["data_situacao_cadastral"], errors='coerce')
+            chunk = chunk.loc[(chunk['situacao_cadastral'] == 2) | (chunk['data_situacao_cadastral'] >= pd.to_datetime('2013-01-01'))].reset_index(drop=True)
             chunk['matriz'] = chunk['identificador_matriz_filial'] == 1
             chunk['mei'] = chunk['opcao_pelo_mei'] == "S"
-            chunk['cep'] = chunk['cep'].astype(
-                str).fillna('').apply(lambda x: x[:8])
-            chunk = chunk.drop(['identificador_matriz_filial', 'opcao_pelo_mei', 'uf'], axis=1).rename(
-                columns=renomear).fillna("NULL")
+            chunk['cep'] = chunk['cep'].astype(str).fillna('').apply(lambda x: x[:8])
+            chunk = chunk.drop(['identificador_matriz_filial', 'opcao_pelo_mei', 'uf'], axis=1).rename(columns=renomear).fillna("NULL")
             chunk['natureza_id'] = chunk['natureza_id'].replace(2100, 2062)
-            chunk['cnae_id'] = chunk['cnae_id'].replace(5812300, 5812301)
-            chunk['cnae_id'] = chunk['cnae_id'].replace(4751200, 4751201)
-            chunk['cnae_id'] = chunk['cnae_id'].replace(1091100, 1091101)
-            chunk['cnae_id'] = chunk['cnae_id'].replace(8888888, 8299799)
+            chunk['cnae_id'] = chunk['cnae_id'].replace({5812300: 5812301, 4751200: 4751201, 1091100: 1091101, 3511500: 3511501,
+                                                         8888888: 8299799, 4721101: 4721102, 2539000: 2539001, 5611202: 5611204, 1822900: 1822999})
+
             if len(chunk) > 0:
                 try:
-                    seeder(sessao=sessao, model=Cnpj, df=chunk)
+                    logging.info("Vou inserir...")
+                    seeder(sessao=sessao, model=Cnpj, df=chunk, logging=logging)
+                    logging.info('dados inseridos!')
                 except IntegrityError as e:
                     logging.error(e)
                     sessao.rollback()
                     break
             del chunk
+
+
+def testaCnaes():
+    arquivo = DIR + "cnpjs.csv"
+    cols = ['codigo_municipio', 'data_situacao_cadastral', 'situacao_cadastral', 'uf']
+    cnaes = set()
+    municipios = set()
+    naturezas = set()
+
+    with pd.read_csv(arquivo, encoding='utf-8', sep='#', chunksize=100_000, usecols=cols) as reader:
+        for cont, chunk in enumerate(reader):
+            logging.info(f'Inserção CNPJs: Processando o chunk {cont}...')
+            chunk = chunk.loc[chunk['uf'].str.upper() != 'EX'].reset_index(drop=True)
+            chunk["data_situacao_cadastral"] = pd.to_datetime(chunk["data_situacao_cadastral"], errors='coerce')
+            chunk = chunk.loc[(chunk['situacao_cadastral'] == 2) | (chunk['data_situacao_cadastral'] >= pd.to_datetime('2013-01-01'))].reset_index(drop=True)
+
+            cnaes = cnaes.union(chunk['cnae_fiscal'].unique())
+            municipios = municipios.union(chunk['codigo_municipio'].unique())
+            naturezas = naturezas.union(chunk['codigo_natureza_juridica'].unique())
+
+            del chunk
+
+    logging.info("Todos os chunks foram processados!")
+
+    municipios_bd = municipios.difference(set(consultaDados(select(Cidade.siafi_id))['siafi_id']))
+    cnaes_bd = cnaes.difference(set(consultaDados(select(Cnae.id))['id']))
+    naturezas_bd = naturezas.difference(set(consultaDados(select(NaturezaJuridica.id))['id']))
+
+    logging.info('Cidades não cadastradas:')
+    logging.info(municipios_bd)
+
+    logging.info('Naturezas não cadastradas:')
+    logging.info(naturezas_bd)
+
+    logging.info('Cnaes não cadastradas:')
+    logging.info(cnaes_bd)
+
+    with open('./dados ausentes.txt', 'w', encoding='utf-8') as fl:
+        fl.write('Cidades: \n')
+        fl.writelines([str(x) + "\n" for x in municipios_bd])
+        fl.write('CNAES: \n')
+        fl.writelines([str(x) + "\n" for x in cnaes_bd])
+        fl.write('Naturezas: \n')
+        fl.writelines([str(x) + "\n" for x in naturezas_bd])
 
 
 if __name__ == '__main__':
